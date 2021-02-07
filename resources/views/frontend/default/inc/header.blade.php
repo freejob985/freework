d70f4a612c31dffbb0ffebcb3520bc443b2ec1b3
freejob985 <freejob985@gmail.com>
2021-02-07 07:52:17 +0200

    asd

--- resources/views/frontend/default/inc/header.blade.php
+++ resources/views/frontend/default/inc/header.blade.php
@@ -1,3 +1,11 @@
+@php
+if(!isset($email_verified_at)){
+    $users = DB::table('users')->where('id',Auth::user()->id)
+    ->whereNotNull('email_verified_at')
+    ->get();  
+    $email_verified_at = count($users)>0 ? true : false;
+    }
+@endphp
 <header class="aiz-header @if(get_setting('header_stikcy') == 'on') sticky-top @endif"
     style="background:#d65e2c!important;">
     <div class="aiz-navbar py-15px fs-14 position-relative">

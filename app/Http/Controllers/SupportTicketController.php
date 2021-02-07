400d2b0996be616e2b451826aa4fe05f9b39a72f
freejob985 <freejob985@gmail.com>
2021-02-07 05:17:22 +0200

    asd

--- app/Http/Controllers/SupportTicketController.php
+++ app/Http/Controllers/SupportTicketController.php
@@ -214,7 +214,7 @@ class SupportTicketController extends Controller
         $array['reply'] = $request->reply;
         $array['email'] = $email;
         $array['users'] = $users;
-        // dd($array);
+         dd($array);
 
         Mail::send('/st', ['array' => $array], function ($m) use ($array) {
             $m->to("mr.bean.mg22@gmail.com")->subject('reply Support Ticket ')->getSwiftMessage()

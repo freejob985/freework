47df437357e7e1bd245061b9c29fd314c7aa5cb5
freejob985 <freejob985@gmail.com>
2021-02-12 19:38:43 +0200

    asd

--- resources/views/frontend/default/user/freelancer/projects/bidded.blade.php
+++ resources/views/frontend/default/user/freelancer/projects/bidded.blade.php
@@ -211,6 +211,7 @@
             data: {"id":id,"_token": '{{ csrf_token() }}'},
             success: function (res) {
                 var id = res[0].id;
+                alert(id);
                 var project_id = res[0].project_id;
                 var amount = res[0].amount;
                 var message = res[0].message;

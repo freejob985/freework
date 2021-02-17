084f9eccd61782e766de12d045e94e70a8a4dfdf
freejob985 <freejob985@gmail.com>
2021-02-17 09:13:49 +0200

    شسي

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\ChatThread;
-use App\Models\Project;
 use App\Models\Chat;
+use App\Models\ChatThread;
 use App\User;
-use App\Models\Role;
-use Session;
 use Auth;
 use Gate;
+use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
@@ -20,7 +17,7 @@ class ChatController extends Controller
         $existing_chat_thread = ChatThread::where('sender_user_id', Auth::user()->id)->where('receiver_user_id', $bidder->id)->first();
         if ($existing_chat_thread == null) {
             $existing_chat_thread = new ChatThread;
-            $existing_chat_thread->thread_code = $bidder->id.date('Ymd').Auth::user()->id;
+            $existing_chat_thread->thread_code = $bidder->id . date('Ymd') . Auth::user()->id;
             $existing_chat_thread->sender_user_id = Auth::user()->id;
             $existing_chat_thread->receiver_user_id = $bidder->id;
             $existing_chat_thread->save();
@@ -34,37 +31,33 @@ class ChatController extends Controller
         $chat_threads = ChatThread::where('sender_user_id', Auth::user()->id)->orWhere('receiver_user_id', Auth::user()->id)->get();
         return view('frontend.default.user.messages', compact('chat_threads'));
     }
-
+//شات
     public function chat_index_($id)
     {
         $chat_threads = ChatThread::where('sender_user_id', Auth::user()->id)->orWhere('receiver_user_id', $id)->get();
         return view('frontend.default.user.messages', compact('chat_threads'));
     }
 
-
     public function admin_chat_index(Request $request)
     {
         if (Gate::allows('user_chat_index')) {
             $sort_search = null;
             $chat_threads = ChatThread::orderBy('created_at', 'desc');
-            if ($request->has('search')){
+            if ($request->has('search')) {
                 $sort_search = $request->search;
-                $user_ids = User::where(function($user) use ($sort_search){
-                    $user->where('name', 'like', '%'.$sort_search.'%')->orWhere('email', 'like', '%'.$sort_search.'%');
+                $user_ids = User::where(function ($user) use ($sort_search) {
+                    $user->where('name', 'like', '%' . $sort_search . '%')->orWhere('email', 'like', '%' . $sort_search . '%');
                 })->pluck('id')->toArray();
-                $chat_threads = $chat_threads->where(function($chat_thread) use ($user_ids){
+                $chat_threads = $chat_threads->where(function ($chat_thread) use ($user_ids) {
                     $chat_thread->whereIn('sender_user_id', $user_ids)->orWhereIn('receiver_user_id', $user_ids);
                 });
                 $chat_threads = $chat_threads->paginate(10);
-            }
-            else {
+            } else {
                 $chat_threads = $chat_threads->paginate(12);
             }
 
-
             return view('admin.default.chats.index', compact('chat_threads', 'sort_search'));
-        }
-        else {
+        } else {
             flash('You do not have access permission')->error();
             return back();
         }
@@ -75,9 +68,8 @@ class ChatController extends Controller
         if (Gate::allows('single_user_chat_details')) {
             $chat_thread = ChatThread::findOrFail(decrypt($id));
             $chats = $chat_thread->chats;
-            return view('admin.default.chats.show', compact('chats','chat_thread'));
-        }
-        else {
+            return view('admin.default.chats.show', compact('chats', 'chat_thread'));
+        } else {
             flash('You do not have access permission')->error();
             return back();
         }
@@ -87,7 +79,7 @@ class ChatController extends Controller
     {
         $chat_thread = ChatThread::findOrFail($id);
         foreach ($chat_thread->chats as $key => $chat) {
-            if($chat->sender_user_id != Auth::user()->id){
+            if ($chat->sender_user_id != Auth::user()->id) {
                 $chat->seen = 1;
                 $chat->save();
             }
@@ -101,29 +93,27 @@ class ChatController extends Controller
     {
         $chat = Chat::findOrFail($request->first_message_id);
         $chats = Chat::where('id', '<', $chat->id)->where('chat_thread_id', $chat->chat_thread_id)->latest()->limit(20)->get();
-        if(count($chats) > 0){
+        if (count($chats) > 0) {
             return array('messages' => view('frontend.default.partials.chat-messages-part', compact('chats'))->render(),
-                         'first_message_id' => $chats->last()->id);
-        }
-        else {
+                'first_message_id' => $chats->last()->id);
+        } else {
             return array('messages' => "",
-                         'first_message_id' => 0);
+                'first_message_id' => 0);
         }
     }
 
     public function chat_refresh($id)
     {
         $chat_thread = ChatThread::findOrFail($id);
-        $chats = $chat_thread->chats()->where('sender_user_id', '!=', Auth::user()->id)->where('seen' , 0)->get();
+        $chats = $chat_thread->chats()->where('sender_user_id', '!=', Auth::user()->id)->where('seen', 0)->get();
         foreach ($chats as $key => $value) {
             $value->seen = 1;
             $value->save();
         }
 
         return array('messages' => view('frontend.default.partials.chat-messages-left-single', compact('chats'))->render(),
-                     'count' => count($chats));
+            'count' => count($chats));
     }
-
 
     public function chat_reply(Request $request)
     {
@@ -131,7 +121,7 @@ class ChatController extends Controller
         $chat->chat_thread_id = $request->chat_thread_id;
         $chat->sender_user_id = Auth::user()->id;
         $chat->message = $request->message;
-        if($request->attachment != null){
+        if ($request->attachment != null) {
             $chat->attachment = json_encode(explode(',', $request->attachment));
         }
         $chat->save();
@@ -143,14 +133,12 @@ class ChatController extends Controller
         $chat_thread = ChatThread::findOrFail($request->chat_thread_id);
         if ($chat_thread->interview == 1) {
             $chat_thread->interview = 0;
-        }
-        else {
+        } else {
             $chat_thread->interview = 1;
         }
         if ($chat_thread->save()) {
             return 1;
-        }
-        else {
+        } else {
             return 0;
         }
     }
@@ -160,14 +148,12 @@ class ChatController extends Controller
         $chat_thread = ChatThread::findOrFail($request->chat_thread_id);
         if ($chat_thread->active == 1) {
             $chat_thread->active = 0;
-        }
-        else {
+        } else {
             $chat_thread->active = 1;
         }
         if ($chat_thread->save()) {
             return 1;
-        }
-        else {
+        } else {
             return 0;
         }
     }

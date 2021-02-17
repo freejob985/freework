5066b691408988deaa36ff69bbca54895a075d46
freejob985 <freejob985@gmail.com>
2021-02-17 15:42:57 +0200

    asd

--- resources/views/frontend/default/Offers.blade.php
+++ resources/views/frontend/default/Offers.blade.php
@@ -171,6 +171,11 @@
 
             </div>
             
+            <div class="btn-group btn-group-justified">
+                <a href="#" class="btn btn-primary">Apple</a>
+                <a href="#" class="btn btn-primary">Samsung</a>
+                <a href="#" class="btn btn-primary">Sony</a>
+              </div> 
         </div>
         <hr>
         @endforeach

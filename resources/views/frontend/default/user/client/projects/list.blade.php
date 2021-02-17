6e335f620cc5a05c3d9809f575f273a5f5a1b38a
freejob985 <freejob985@gmail.com>
2021-02-17 03:35:41 +0200

    شسي

--- resources/views/frontend/default/user/client/projects/list.blade.php
+++ resources/views/frontend/default/user/client/projects/list.blade.php
@@ -206,8 +206,8 @@
 		                                    </div>
 		                                </a>
 										<div>
-											<a href="javascript:void(0)" class="btn btn-secondary btn-sm fw-500 confirm-cancel" data-href="{{route('projects.cancel', $project->id)}}">{{ translate('Cancel') }}</a>
-	                                        <a href="javascript:void(0)" class="btn btn-primary btn-sm fw-500 confirm-complete" data-href="{{route('projects.complete', $project->id)}}">{{ translate('Complete This') }}</a>
+											<a href="javascript:void(0)" class="btn btn-secondary btn-sm fw-500 confirm-cancel" data-href="{{route('projects.my_cancelled_project', $project->id)}}">{{ translate('Cancel') }}</a>
+	                                        <a href="javascript:void(0)" class="btn btn-primary btn-sm fw-500 confirm-complete" data-href="{{route('projects.my_completed_project', $project->id)}}">{{ translate('Complete This') }}</a>
 										</div>
 									@else
 										<div class="d-flex align-items-center">

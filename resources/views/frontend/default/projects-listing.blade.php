cdda5986db2419fa53ff9f06f292ecce72c054be
freejob985 <freejob985@gmail.com>
2021-02-17 04:33:47 +0200

    asd

--- resources/views/frontend/default/projects-listing.blade.php
+++ resources/views/frontend/default/projects-listing.blade.php
@@ -223,11 +223,11 @@
                                                 </div>
 
                                                 <div class="mb-0">
-                                                  
+                                                    <span>مدة التنيفذ &nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="1" stroke-linecap="square" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg></span>
+
                                 
                                                         <span class="text-body mr-1">يوم       &nbsp;{{ $project->execute }} </span>
                                      
-                                                    <span>مدة التنيفذ &nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="1" stroke-linecap="square" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg></span>
                                                 </div>
                                             </div>
                                             <div>

4a2726ad2a9ed173e33f18b3853ced9fe0b596ce
freejob985 <freejob985@gmail.com>
2021-02-19 03:37:02 +0200

    asd

--- resources/views/support_ticket/frontend/support_ticket_show.blade.php
+++ resources/views/support_ticket/frontend/support_ticket_show.blade.php
@@ -173,7 +173,7 @@
                                     </div>
                                 </li>
                             </ul>
-                            @if ($support_ticket->status=="1" and empty($support_ticket->rating) )
+                            @if ($support_ticket->status=="1" and $support_ticket->st=="0" )
                                 
                        
                             <form action="{{ route('support-ticket.reviews') }}" method="POST" enctype="multipart/form-data">

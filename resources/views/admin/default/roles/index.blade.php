dcadb6ed4da6f7a85d8c1823df88be165100ddda
freejob985 <freejob985@gmail.com>
2021-02-21 18:20:08 +0200

    asd

--- resources/views/admin/default/roles/index.blade.php
+++ resources/views/admin/default/roles/index.blade.php
@@ -1,6 +1,9 @@
 @extends('admin.default.layouts.app')
 
 @section('content')
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <style>
     .form-group input[type="checkbox"] {
         display: none;

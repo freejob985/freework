a1ebfc9a3e449a3cc2ff7e46826ac711735df326
freejob985 <freejob985@gmail.com>
2021-02-23 21:43:55 +0200

    asd

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -33,9 +33,15 @@ class ChatController extends Controller
         
                 return redirect()->route('all.messages',['id'=> $bidder->id]);
         }
+     
+    
+      
+
+
+
         }
+
         else {
-        //    dd("Freelanser");
             $bidder = User::where('user_name', $request->user_name)->first();
             if (DB::table('chat_threads')->where('receiver_user_id',  $bidder->id)->exists()) {
                 return redirect()->route('all.messages.user', ['id'=> $bidder->id]);
@@ -44,12 +50,12 @@ class ChatController extends Controller
                 if ($existing_chat_thread == null) {
                     $existing_chat_thread = new ChatThread;
                     $existing_chat_thread->thread_code = $bidder->id.date('Ymd').Auth::user()->id;
-                    $existing_chat_thread->sender_user_id =  $bidder->id;
-                    $existing_chat_thread->receiver_user_id =Auth::user()->id; 
+                    $existing_chat_thread->sender_user_id = Auth::user()->id;
+                    $existing_chat_thread->receiver_user_id = $bidder->id;
                     $existing_chat_thread->save();
                 }
+        
                 return redirect()->route('all.messages',['id'=> $bidder->id]);
-        }
         }
 
        

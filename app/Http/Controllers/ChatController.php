7a1dd39fbfc659b1d9418738c984e1bd3ab441d8
freejob985 <freejob985@gmail.com>
2021-02-24 17:20:26 +0200

    شسي

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -46,7 +46,7 @@ class ChatController extends Controller
             if (DB::table('chat_threads')->where('receiver_user_id',  $bidder->id)->exists()) {
                 return redirect()->route('all.messages.user', ['id'=> $bidder->id]);
             }else{
-                $existing_chat_thread = ChatThread::where('sender_user_id', Auth::user()->id)->where('receiver_user_id', $bidder->id)->first();
+                $existing_chat_thread = ChatThread::where('sender_user_id', $bidder->id)->where('receiver_user_id', $bidder->id)->first();
                 if ($existing_chat_thread == null) {
                     $existing_chat_thread = new ChatThread;
                     $existing_chat_thread->thread_code = $bidder->id.date('Ymd').Auth::user()->id;

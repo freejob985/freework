a47348b52279f6172e71c2a03ddbb4ac4a187a83
freejob985 <freejob985@gmail.com>
2021-02-25 14:10:13 +0200

    asd

--- resources/views/frontend/default/user/freelancer/projects/private.blade.php
+++ resources/views/frontend/default/user/freelancer/projects/private.blade.php
@@ -79,11 +79,11 @@
                                             
                                             <a href="{{ route('hiring.reject', encrypt($private_project->id)) }}" class="btn btn-danger btn-sm">{{ translate('Reject') }}</a>
                                             <a href="javascript:void(0)" class="btn btn-success btn-sm" onclick="hiring_modal('{{ $private_project->project->name }}',{{ $private_project->project->price }}, {{ $private_project->project->id }}, {{ Auth::user()->id }})">{{ translate('Accpet') }}</a>
-                                            <form class="mt-2" action="{{ route('all.messages',['id'=> Auth::user()->id]) }}" method="post">
+                                            <form class="mt-2" action="{{ route('call_for_interview') }}" method="post">
                                                 <input type="hidden" id="user_name" name="user_name"
                                                 value="{{ get_current_user__( Auth::user()->id,"user_name") }}">
 
-                                                
+
                                             <button   type="submit"  class="btn btn-primary btn-sm">{{ translate('Chat With Client') }}</button>
                                         </form>
                                         </div>
